a5e7a9616bacd21237dbab5b09168ec86def3b89
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-04-29 10:19:41 +0100

    Star rating

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Accordian from './components/accordian/Accordian';
 import RandomColour from './components/randomColour/randomColour';
+import StarRating from './components/starRating/StarRating';
 
 function App() {
   return (
     <div className="App">
       {/* <Accordian /> */}
-      <RandomColour />
+      {/* <RandomColour /> */}
+      <StarRating />
     </div>
   );
 }
