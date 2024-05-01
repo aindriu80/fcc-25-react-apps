89e89577a52cf33d000d3d2fff1539f9acd2d928
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-05-01 10:03:22 +0100

    Load More Data

--- src/components/LoadMoreData/LoadMoreData.tsx
+++ src/components/LoadMoreData/LoadMoreData.tsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import './style.css';
 
 const LoadMoreData = () => {
-  return <div>loadMoreData</div>;
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
+
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=10&skip=${count === 0 ? 0 : count * 20}&select=title,price,thumbnail`,
+      );
+
+      const result = await response.json();
+
+      if (result && result.products && result.products.length) {
+        setProducts((prevData) => [...prevData, ...result.products]);
+        setLoading(false);
+      }
+
+      console.log('result', result.products);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [count]);
+
+  useEffect(() => {
+    if (products && products.length === 100) setDisableButton(true);
+  }, [products]);
+
+  if (loading) {
+    return <div>Loading data! Please wait....</div>;
+  }
+
+  return (
+    <div className="container">
+      <div className="product-container">
+        {products && products.length
+          ? products.map((item: Array) => (
+              <div className="product" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+
+      <div className="button-container">
+        <button disabled={disableButton} onClick={() => setCount(count + 1)}>
+          Load more products
+        </button>
+        {disableButton ? (
+          <p>You have reached the maximum 100 products</p>
+        ) : null}
+      </div>
+    </div>
+  );
 };
 
 export default LoadMoreData;
