a48cc4a59cccadc5c5ced1971ab1a56f6105ad54
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-05-21 12:15:38 +0100

    API Call

--- src/components/WeatherApplication/WeatherApplication.tsx
+++ src/components/WeatherApplication/WeatherApplication.tsx
@@ -1,10 +1,37 @@
 import { useState } from 'react';
 import SearchComponent from './SearchComponent';
 
+const WeatherAPIKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
+
 const WeatherApplication = () => {
   const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
 
-  async function handleSearch() {}
+  async function fetchWeatherData(param) {
+    setLoading(true);
+    console.log(WeatherAPIKey);
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=${WeatherAPIKey}`,
+      );
+
+      const data = await response.json();
+
+      if (data) {
+        setWeatherData(data);
+        setLoading(false);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }
+  console.log(loading);
+
+  function handleSearch() {
+    fetchWeatherData(search);
+  }
 
   return (
     <div>
