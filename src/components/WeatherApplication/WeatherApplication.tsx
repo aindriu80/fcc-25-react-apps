ac6bff09d54a8167a4d535ed7f2140107e7361ae
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-05-22 15:42:27 +0100

    Specifying items to return from API object

--- src/components/WeatherApplication/WeatherApplication.tsx
+++ src/components/WeatherApplication/WeatherApplication.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SearchComponent from './SearchComponent';
 
 const WeatherAPIKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
@@ -10,13 +10,14 @@ const WeatherApplication = () => {
 
   async function fetchWeatherData(param) {
     setLoading(true);
-    console.log(WeatherAPIKey);
+
     try {
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=${WeatherAPIKey}`,
       );
 
       const data = await response.json();
+      console.log('data', data);
 
       if (data) {
         setWeatherData(data);
@@ -27,11 +28,23 @@ const WeatherApplication = () => {
       console.log(error);
     }
   }
-  console.log(loading);
 
   function handleSearch() {
     fetchWeatherData(search);
   }
+
+  function getCurrentDate() {
+    return new Date().toLocaleDateString('ga-ie', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    });
+  }
+
+  useEffect(() => {
+    fetchWeatherData('Dublin, IE');
+  }, []);
 
   return (
     <div>
@@ -40,7 +53,26 @@ const WeatherApplication = () => {
         setSearch={setSearch}
         handleSearch={handleSearch}
       />
-      Weather Application
+      {loading ? (
+        <div>Loading....</div>
+      ) : (
+        <div className="">
+          <div className="city-name">
+            <h2>
+              {weatherData?.name},<span>{weatherData?.sys.country}</span>
+            </h2>
+          </div>
+          <div className="date">
+            <span>{getCurrentDate()}</span>
+          </div>
+          <div>{weatherData?.main?.temp}</div>
+          <p className="description">
+            {weatherData && weatherData.weather && weatherData.weather[0]
+              ? weatherData.weather[0].description
+              : ''}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
