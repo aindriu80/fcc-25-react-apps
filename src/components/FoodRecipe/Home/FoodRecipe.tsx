02487c017397ed049351d51be0d604a7703f0c07
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-05-25 17:25:32 +0100

    Food Recipe - Some routing

--- src/components/FoodRecipe/Home/FoodRecipe.tsx
+++ src/components/FoodRecipe/Home/FoodRecipe.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Details from '../Details/Details';
+import Favorites from '../Favorites/Favorites';
+import Navbar from '../Navbar/Navbar';
 
 const FoodRecipe = () => {
-  return <div>FoodRecipe</div>
-}
+  return (
+    <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+      <Navbar />
+      <Routes>
+        {/* <Route path="/" element={<FoodRecipe />} /> */}
+        <Route path="/recipe-item/:id" element={<Details />}></Route>
+        <Route path="/" element={<Favorites />}></Route>
+      </Routes>
+      FoodRecipe
+    </div>
+  );
+};
 
-export default FoodRecipe
+export default FoodRecipe;
