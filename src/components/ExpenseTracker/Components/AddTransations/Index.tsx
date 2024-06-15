97cc088217279dd5875bc1865184b91aff090723
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-06-15 21:40:32 +0100

    More context work

--- src/components/ExpenseTracker/Components/AddTransations/Index.tsx
+++ src/components/ExpenseTracker/Components/AddTransations/Index.tsx
@@ -13,47 +13,81 @@ import {
   Radio,
   RadioGroup,
 } from '@chakra-ui/react';
+import { useContext } from 'react';
+import { GlobalContext } from '../../Context/Index';
 
 const TransactionForm = ({ onClose, isOpen }) => {
+  const { formData, setFormData, value, setValue, handleFormSubmit } =
+    useContext(GlobalContext);
+
+  function handleFormChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleFormSubmit(formData);
+  }
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Add New Transaction</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <FormControl>
-            <FormLabel>Enter Description</FormLabel>
-            <Input
-              placeholder="Enter transaction description"
-              name="description"
-              type="text"
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Enter Amount</FormLabel>
-            <Input
-              placeholder="Enter transaction amount"
-              name="amount"
-              type="number"
-            />
-          </FormControl>
-          <RadioGroup mt="5">
-            <Radio value="income" colorScheme="blue" name="type">
-              Expense
-            </Radio>
-            <Radio value="expense" colorScheme="red" name="type">
-              Income
-            </Radio>
-          </RadioGroup>
-        </ModalBody>
-        <ModalFooter>
-          <Button mr={'4'} onClick={onClose}>
-            Cancel
-          </Button>
-          <Button>Add</Button>
-        </ModalFooter>
-      </ModalContent>
+      <form onSubmit={handleSubmit}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add New Transaction</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Enter Description</FormLabel>
+              <Input
+                placeholder="Enter transaction description"
+                name="description"
+                type="text"
+                onChange={handleFormChange}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Enter Amount</FormLabel>
+              <Input
+                placeholder="Enter transaction amount"
+                name="amount"
+                type="number"
+              />
+            </FormControl>
+            <RadioGroup mt="5" value={value} onChange={setValue}>
+              <Radio
+                checked={formData.type === 'income'}
+                value="income"
+                colorScheme="blue"
+                name="type"
+                onChange={handleFormChange}
+              >
+                Income
+              </Radio>
+              <Radio
+                checked={formData.type === 'expense'}
+                value="expense"
+                colorScheme="red"
+                name="type"
+                onChange={handleFormChange}
+              >
+                Expense
+              </Radio>
+            </RadioGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button mr={'4'} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button onClick={onClose} type="submit">
+              Add
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </form>
     </Modal>
   );
 };
--- src/components/ExpenseTracker/Context/Index.tsx
+++ src/components/ExpenseTracker/Context/Index.tsx
@@ -1,7 +1,46 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
-  return <GlobalContext.Provider>{children}</GlobalContext.Provider>;
+  const [formData, setFormData] = useState({
+    type: 'expense',
+    amount: 0,
+    description: '',
+  });
+
+  const [value, setValue] = useState('');
+  const [totalExpense, setTotalExpense] = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [allTransations, setAllTransactions] = useState([]);
+
+  function handleFormSubmit(currentFormData) {
+    if (!currentFormData.description || !currentFormData.amount) return;
+
+    setAllTransactions([
+      ...allTransations,
+      { ...currentFormData, id: Date.now() },
+    ]);
+  }
+  console.log(allTransations);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        formData,
+        setFormData,
+        totalExpense,
+        setTotalExpense,
+        totalIncome,
+        setTotalIncome,
+        value,
+        setValue,
+        allTransations,
+        setAllTransactions,
+        handleFormSubmit,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 }
