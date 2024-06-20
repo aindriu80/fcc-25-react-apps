df7f5ba53c4fd2a862a839965118e210ac38020f
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-06-20 15:57:14 +0100

    Creating the controller and model

---
+++ src/components/MernBlog/server/controller/Blog-Controller.js
@@ -0,0 +1,96 @@
+const mongoose = require('mongoose');
+const Blog = require('../model/Blog');
+
+// Required controllers ->
+// fetch list of blogs
+// add a new blog
+// delete a blog
+// update a blog
+
+const fetchListOfBlogs = async (req, res) => {
+  let blogList;
+  try {
+    blogList = await Blog.find();
+  } catch (e) {
+    console.log(e);
+  }
+
+  if (!blogList) {
+    return res.status(404).json({ message: 'No Blogs Found' });
+  }
+
+  return res.status(200).json({ blogList });
+};
+
+const addNewBlog = async (req, res) => {
+  const { title, description } = req.body;
+  const currentDate = new Date();
+  const newlyCreatedBlog = new Blog({
+    title,
+    description,
+    date: currentDate,
+  });
+
+  try {
+    await newlyCreatedBlog.save();
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    await newlyCreatedBlog.save(session);
+    session.commitTransaction();
+  } catch (error) {
+    return res.send(500).json({ message: error });
+  }
+
+  return res.status(200).json({ newlyCreatedBlog });
+};
+
+const deleteABlog = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const findCurrentBlog = await Blog.findByIdAndDelete(id);
+    if (!findCurrentBlog) {
+      return res.status(404).json({ message: 'Blog not found' });
+    }
+
+    return res.status(200).json({ message: 'Successfully Deleted' });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: 'Unable to delete! Please try again' });
+  }
+};
+
+const updateABlog = async (req, res) => {
+  const id = req.params.id;
+
+  const { title, description } = req.body;
+  let currentBlogToUpdate;
+
+  try {
+    currentBlogToUpdate = await Blog.findByIdAndUpdate(id, {
+      title,
+      description,
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.send(500).json({
+      message: 'Something went wrong while updating, please try again',
+    });
+
+    if (!currentBlogToUpdate) {
+      return res.status(500).json({ message: 'Unable to update' });
+    }
+
+    return res.send(200).json({ message: 'Unable to update' });
+  }
+};
+
+modules.exports = { fetchListOfBlogs, deleteABlog, updateABlog, addNewBlog };
