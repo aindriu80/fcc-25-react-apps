5585d220e1386512c194ce08f0c6fea589f7fa1f
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-06-25 10:49:21 +0100

    Displaying Blogs

--- src/components/MernBlog/client/src/pages/home/index.jsx
+++ src/components/MernBlog/client/src/pages/home/index.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useContext } from 'react';
+import { GlobalContext } from '../../context';
+import axios from 'axios';
 
 const Home = () => {
-  return <h1>Blog List</h1>;
+  const { blogList, setBlogList, pending, setPending } =
+    useContext(GlobalContext);
+
+  async function fetchListOfBlogs() {
+    setPending(true);
+    const response = await axios.get('http://localhost:5000/api/blogs');
+    const result = await response.data;
+
+    console.log(result);
+
+    if (result && result.blogList && result.blogList.length) {
+      setBlogList(result.blogList);
+      setPending(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchListOfBlogs();
+  }, []);
+
+  return (
+    <div>
+      <h1>Blog List</h1>
+      {pending ? (
+        <h1>Loading Blogs! Please wait</h1>
+      ) : (
+        <div>
+          {blogList.map((blogItem) => (
+            <div key={blogItem._id}>
+              <p>{blogItem.title}</p>
+              <p>{blogItem.description}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
