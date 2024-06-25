1a35da5c164806d559188f916d3c5a4e9f7eb55f
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-06-25 11:18:10 +0100

    Delete functionality

--- src/components/MernBlog/client/src/pages/home/index.jsx
+++ src/components/MernBlog/client/src/pages/home/index.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useContext } from 'react';
 import { GlobalContext } from '../../context';
 import axios from 'axios';
+import classes from './styles.module.css';
+import { FaTrash, FaEdit } from 'react-icons/fa';
 
 const Home = () => {
   const { blogList, setBlogList, pending, setPending } =
@@ -12,31 +14,59 @@ const Home = () => {
     const response = await axios.get('http://localhost:5000/api/blogs');
     const result = await response.data;
 
-    console.log(result);
-
     if (result && result.blogList && result.blogList.length) {
       setBlogList(result.blogList);
       setPending(false);
+    } else {
+      setPending(false);
+      setBlogList([]);
     }
   }
+
+  async function handleDeleteBlog(getCurrentId) {
+    console.log('currentId: ', getCurrentId);
+
+    const response = await axios.delete(
+      `http://localhost:5000/api/blogs/delete/${getCurrentId}`,
+    );
+    const result = await response.data;
+
+    if (result?.message) {
+      fetchListOfBlogs();
+    }
+  }
+
+  async function handleEditBlog() {}
 
   useEffect(() => {
     fetchListOfBlogs();
   }, []);
 
   return (
-    <div>
+    <div className={classes.wrapper}>
       <h1>Blog List</h1>
       {pending ? (
         <h1>Loading Blogs! Please wait</h1>
       ) : (
-        <div>
-          {blogList.map((blogItem) => (
-            <div key={blogItem._id}>
-              <p>{blogItem.title}</p>
-              <p>{blogItem.description}</p>
-            </div>
-          ))}
+        <div className={classes.blogList}>
+          {blogList && blogList.length ? (
+            blogList.map((blogItem) => (
+              <div key={blogItem._id}>
+                <p>{blogItem.title}</p>
+                <p>{blogItem.description}</p>
+                <FaEdit
+                  onClick={() => handleEditBlog(blogItem._id)}
+                  size={30}
+                />
+                <FaTrash
+                  size={30}
+                  onClick={() => handleDeleteBlog(blogItem._id)}
+                />
+              </div>
+            ))
+          ) : (
+            <h3>No Blogs Added</h3>
+          )}
         </div>
       )}
     </div>
